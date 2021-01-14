9dcfe21e2a8f416c2c24b139eb10d1be0c898068
piotrmikula <p.mikula14@wp.pl>
2021-01-14 12:44:06 +0000

    Zadanie4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,20 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+obliczMake(const std::vector<Tagliatelle>& tvec)
+{
+  double M = 0;
+  int size = tvec.size();
+  for(auto element : tvec)
+  {
+    M += (element.ileMaki(size));
+    size--;
+  }
+
+  if(M > 100)
+    throw(14);
+  else if(M > 50)
+    throw(0.5);
+  else
+    return M;
+}
