389c7b877b4816edd799290e0a1740ff7ca86c36
piotrmikula <p.mikula14@wp.pl>
2021-01-14 12:52:12 +0000

    Zadanie5

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -7,3 +7,7 @@
 #include <vector>
 
 // tutaj funkcja sortujTagliatelle
+void sortujTagliatelle(T t1, T t2)
+{
+	std::sort(t1, t2, [](Tagliatelle& taglia1, Tagliatelle& taglia2) { return taglia1.ileMaki(1) > taglia2.ileMaki(1); });
+}
