0fe30c7d669a60165a8ada5e9af5cd71224455a3
piotrmikula <p.mikula14@wp.pl>
2021-01-14 12:31:34 +0000

    Zadanie2v6

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -2,32 +2,28 @@
 
 // Zad2
 // tutaj definicja klasy Makaron
-// BEZ definicji metody gotujMakaron (tylko z deklaracją)
-class Makaron
-{
-  public:
+// BEZ definicji metody gotujMakaron (tylko z deklaracja)
+class Makaron 
+{ 
+public:
+    static Makaron* gotujMakaron(const std::string& str);
     virtual double ileMaki(unsigned P) const = 0;
-    static Makaron* gotujMakaron(const std::string& str)
 };
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
 class Tagliatelle : public Makaron
 {
-  public:
-  Tagliatelle(): L{0.5}, W{0.5}, R{0.5} {}
-  Tagliatelle(double l, double w, double r): L{l}, W{w}, R{r} {}
-  
-  //zwracającą masę mąki potrzebną do przygotowania liczby porcji równej argumentowi metody
-  double ileMaki(unsigned P) const override
-  {
-    return P * L * W * (1. - R) * C;
-  }
-
-  private:
-  double L; //dlugosc makaronu
-  double W; //szerokosc kawalka makaronu
-  double R; //propocja jajek do maki
-  
-  static const double C;  //pewna stala
+public:
+    Tagliatelle(double l, double w, double r) : L{l}, W{w}, R{r} {}
+    Tagliatelle() : L{0.5}, W{0.5}, R{0.5} {}
+    double ileMaki(unsigned P) const override
+    {
+        return P * L * W * (1. - R) * C;
+    }
+private:
+    double L; //dlugosc makaronu
+    double W; //szerokosc kawalka makaronu
+    double R; //propocja jajek do maki
+    static const double C;  //pewna stala
 };
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -7,19 +7,18 @@
 // tutaj klasa Penne
 class Penne : public Makaron
 {
-  public:
-    double ileMaki(unsigned P) const override
-  {
-    return (double)P;
-  }
-  private:
+	double ileMaki(unsigned P) const override
+	{
+		return (double)P;
+	}
 };
 
 // tutaj definicja metody gotujMakaron
 Makaron* Makaron::gotujMakaron(const std::string& str)
 {
-  if(str.front() == str.back())
-    return new Tagliatelle{3.14, 0.42, 0.1} 
-  else
-    return new Penne{}
+	if(str.front() == str.back())
+	{
+		return new Tagliatelle{3.14, 0.42, 0.1};
+	}
+	else return new Penne{};
 }
