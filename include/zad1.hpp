acf56fb7628c3a261c166113453d5872bf22a932
piotrmikula <p.mikula14@wp.pl>
2021-01-14 12:24:13 +0000

    Zadanie2v5

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -8,21 +8,18 @@ class Makaron
   public:
     virtual double ileMaki(unsigned P) const = 0;
     static Makaron* gotujMakaron(const std::string& str)
-  private:
-
 };
-
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
 class Tagliatelle : public Makaron
 {
   public:
-  Tagliatelle(): L(0.5), W(0.5), R(0.5) {}
-  Tagliatelle(double l, double w, double r): L(l), W(w), R(r) {}
+  Tagliatelle(): L{0.5}, W{0.5}, R{0.5} {}
+  Tagliatelle(double l, double w, double r): L{l}, W{w}, R{r} {}
   
   //zwracającą masę mąki potrzebną do przygotowania liczby porcji równej argumentowi metody
-  virtual double ileMaki(unsigned P) const override
+  double ileMaki(unsigned P) const override
   {
     return P * L * W * (1. - R) * C;
   }
@@ -33,5 +30,4 @@ class Tagliatelle : public Makaron
   double R; //propocja jajek do maki
   
   static const double C;  //pewna stala
-
 };
