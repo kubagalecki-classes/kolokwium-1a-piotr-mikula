8a43ccf9e4c1172228e51f402fd569bcb19c23cf
piotrmikula <p.mikula14@wp.pl>
2021-01-14 11:35:35 +0000

    Zadanie1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,23 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle
+{
+  public:
+  Tagliatelle(): L(0.5), W(0.5), R(0.5) {}
+  Tagliatelle(double l, double w, double r): L(l), W(w), R(r) {}
+  
+  //zwracającą masę mąki potrzebną do przygotowania liczby porcji równej argumentowi metody
+  double ileMaki(unsigned double P) const
+  {
+    return P * L * W * (1. - R) * C;
+  }
+
+  private:
+  double L; //dlugosc makaronu
+  double W; //szerokosc kawalka makaronu
+  double R; //propocja jajek do maki
+  
+  static const double C;  //pewna stala
+
+};
