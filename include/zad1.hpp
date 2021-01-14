bd5492d37d5ebd87de2448a178cf6539a4046555
piotrmikula <p.mikula14@wp.pl>
2021-01-14 12:02:24 +0000

    Zadanie2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -3,17 +3,31 @@
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
+class Makaron
+{
+  public:
+    virtual ~Makaron{}
+
+    virtual double ileMaki(unsigned) const = 0;
+    static Makaron* gotujMakaron(const std::string&)
+  private:
+
+};
+
+/*
+Na koniec dokonaj modyfikacji klasy Makaron potrzebnej do poprawnego usuwania obiektów klas Tagliatelle i Penne przez wskaźnik na obiekt Makaron.
+*/
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
-class Tagliatelle
+class Tagliatelle : public Makaron
 {
   public:
   Tagliatelle(): L(0.5), W(0.5), R(0.5) {}
   Tagliatelle(double l, double w, double r): L(l), W(w), R(r) {}
   
   //zwracającą masę mąki potrzebną do przygotowania liczby porcji równej argumentowi metody
-  double ileMaki(unsigned P) const
+  virtual double ileMaki(unsigned P) const override
   {
     return P * L * W * (1. - R) * C;
   }
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,31 @@
 #include <type_traits>
 
 // tutaj klasa Penne
+class Penne : public Makaron
+{
+  public:
+    virtual double ileMaki(unsigned P) const override
+  {
+    return (double)P;
+  }
+  private:
+};
 
 // tutaj definicja metody gotujMakaron
+static Makaron* Makaron::gotujMakaron(const std::string& str)
+{
+  auto it = str.front();
+  auto it2 = str.back();
+
+  if(it == it2)
+    return new Tagliatelle{3.14, 0.42, 0.1} 
+  else
+    return new Penne{}
+}
+
+
+/*
+
+
+
+*/
